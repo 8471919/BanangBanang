785847e939f3ecbf84ab6a020be37764f4e69ad3
Michael <7471919@naver.com>
2023-01-25 17:54:15 +0900

    setting: connect typeorm with database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: process.env.NODE_ENV === 'prod' ? 10 : 1,
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        entities: [
+          path.join(__dirname, 'src/entities/**/*.entity.ts'),
+          path.join(__dirname, 'dist/entities/**/*.entity.js'),
+        ],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
