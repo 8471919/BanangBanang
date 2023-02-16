fe2a6bbd1cc343ab5e0d51e11fbf431875702ad1
Michael <7471919@naver.com>
2023-02-16 18:49:43 +0900

    setting: set up swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setUpSwagger } from './settings/swagger/init.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+
+  setUpSwagger(app);
+
   await app.listen(port);
   console.log(`Server is open on port ${port}`);
 }
