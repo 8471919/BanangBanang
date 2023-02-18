60d7f126cd039d1fb7ae5c704ba3ae9d3dac711b
Michael <7471919@naver.com>
2023-02-18 20:30:02 +0900

    setting: session setting using express-session and redis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setUpSession } from './settings/session/init.session';
 import { setUpSwagger } from './settings/swagger/init.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+
+  setUpSession(app);
 
   setUpSwagger(app);
 
---
+++ server/src/settings/session/init.session.ts
@@ -0,0 +1,36 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import { Redis } from 'ioredis';
+
+export function setUpSession(app: INestApplication): void {
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const port = configService.get('REDIS_PORT');
+  const host = configService.get('REDIS_HOST');
+
+  const RedisStore = connectRedis(session);
+
+  const client = new Redis({
+    host,
+    port,
+  });
+
+  app.use(
+    session({
+      secret: configService.get('SESSION_SECRET'),
+      saveUninitialized: false,
+      resave: false,
+      store: new RedisStore({
+        client: client,
+        ttl: 30,
+      }),
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 30000,
+      },
+    }),
+  );
+}
