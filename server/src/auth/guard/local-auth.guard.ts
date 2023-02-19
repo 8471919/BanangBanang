b97ef5d0f82c2de285f36f7494327876f6129839
Michael <7471919@naver.com>
2023-02-19 15:34:08 +0900

    chore: basic setting for register

--- server/src/auth/guard/local-auth.guard.ts
+++ server/src/auth/guard/local-auth.guard.ts
@@ -1,12 +1,17 @@
-import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+@Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('here is LocalAuthGuard1');
     const can = await super.canActivate(context);
+    console.log('here is LocalAuthGuard2');
     if (can) {
       const request = context.switchToHttp().getRequest();
+      console.log('here is LocalAuthGuard3');
       await super.logIn(request);
+      console.log('here is LocalAuthGuard4');
     }
     return true;
   }
--- server/src/common/error-message.ts
+++ server/src/common/error-message.ts
@@ -2,4 +2,5 @@ export const ERROR_MESSAGE = {
   FAIL_TO_CREATE_USER: { code: 400, message: '회원가입에 실패했어요!' },
   FAIL_TO_FIND_EMAIL: { code: 401, message: '등록된 이메일이 아닙니다.' },
   FAIL_TO_LOGIN: { code: 403, message: '이메일에 맞는 비밀번호가 아닙니다.' },
+  FAIL_TO_REGISTER_EMAIL: { code: 402, message: '이미 가입된 이메일입니다.' },
 };
