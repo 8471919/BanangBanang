62c0310a221a7b8080372e0cd36b6095dc21dc4d
Michael <7471919@naver.com>
2023-03-06 18:20:29 +0900

    fix: modify article-repository.outboundport.ts

--- server/src/outbound-ports/article/article-repository.outbound-port.ts
+++ server/src/outbound-ports/article/article-repository.outbound-port.ts
@@ -1,21 +1,36 @@
 export const ARTICLE_REPOSITORY_OUTBOUND_PORT =
   'ARTICLE_REPOSITORY_OUTBOUND_PORT' as const;
 
-export type SaveArticleOutboundPortInputDto = {
+export type SaveCommonArticleOutboundPortInputDto = {
   title: string;
   content: string;
   userId: string;
   articleAreaId: number;
   articleTypeId: number;
-  companyName?: string;
-  expirationDate?: string;
 };
-export type SaveArticleOutboundPortOutputDto = {
+export type SaveCommonArticleOutboundPortOutputDto = {
+  articleId: string;
+};
+
+export type SaveJogPostingOutboundPortInputDto = {
+  title: string;
+  content: string;
+  userId: string;
+  articleAreaId: number;
+  articleTypeId: number;
+  companyName: string;
+  expirationDate: string;
+};
+export type SaveJobPostingOutboundPortOutputDto = {
   articleId: string;
 };
 
 export interface ArticleRepositoryOutboundPort {
-  saveArticle(
-    params: SaveArticleOutboundPortInputDto,
-  ): Promise<SaveArticleOutboundPortOutputDto>;
+  saveCommonArticle(
+    params: SaveCommonArticleOutboundPortInputDto,
+  ): Promise<SaveCommonArticleOutboundPortOutputDto>;
+
+  saveJobPosting(
+    params: SaveJogPostingOutboundPortInputDto,
+  ): Promise<SaveJobPostingOutboundPortOutputDto>;
 }
