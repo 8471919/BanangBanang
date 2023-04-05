eb8f7e5adc36ab7bbd174d18ab29c2b16906b5c8
Michael <7471919@naver.com>
2023-04-05 19:29:40 +0900

    docs: add swagger to Comment Controller

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoggedInGuard } from 'src/auth/guard/logged-in.guard';
 import {
   COMMENT_CONTROLLER_INBOUND_PORT,
@@ -7,6 +7,7 @@ import {
   CreateCommentInboundPortInputDto,
 } from 'src/inbound-ports/comment/comment-controller.inbound-port';
 
+@ApiTags('댓글 API')
 @Controller('api/comments')
 export class CommentController {
   constructor(
@@ -14,6 +15,21 @@ export class CommentController {
     private readonly commentControllerInboundPort: CommentControllerInboundPort,
   ) {}
 
+  @ApiOperation({
+    summary: '댓글 작성 api',
+    description: '게시글에 대한 댓글을 생성',
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        parentId: { default: null },
+        articleId: { default: 2 },
+        content: { default: 'test comment' },
+        depth: { default: 0 },
+        userId: { default: 1 },
+      },
+    },
+  })
   @UseGuards(LoggedInGuard)
   @Post()
   async createComment(@Body() body: CreateCommentInboundPortInputDto) {
