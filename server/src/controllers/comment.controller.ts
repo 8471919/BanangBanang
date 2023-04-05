abc44d41c2e5422d8ae4ad5b43106bdd93dfa359
Michael <7471919@naver.com>
2023-04-05 19:28:01 +0900

    feat: Comment Controller

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { LoggedInGuard } from 'src/auth/guard/logged-in.guard';
+import {
+  COMMENT_CONTROLLER_INBOUND_PORT,
+  CommentControllerInboundPort,
+  CreateCommentInboundPortInputDto,
+} from 'src/inbound-ports/comment/comment-controller.inbound-port';
 
 @Controller('api/comments')
-export class CommentController {}
+export class CommentController {
+  constructor(
+    @Inject(COMMENT_CONTROLLER_INBOUND_PORT)
+    private readonly commentControllerInboundPort: CommentControllerInboundPort,
+  ) {}
+
+  @UseGuards(LoggedInGuard)
+  @Post()
+  async createComment(@Body() body: CreateCommentInboundPortInputDto) {
+    const comment = await this.commentControllerInboundPort.createComment(body);
+
+    return comment;
+  }
+}
