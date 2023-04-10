3362d889ea3848326c492a3cc6f8587f899e2d9f
Michael <7471919@naver.com>
2023-04-10 16:03:26 +0900

    feat: readCommentsByUserId in UserController

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,8 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param, UseGuards } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { LoggedInGuard } from 'src/auth/guard/logged-in.guard';
+import { Users } from 'src/decorators/user.decorator';
+import { GetUserIdDto } from 'src/dtos/auth/get.user-id.dto';
 import {
   UserControllerInboundPort,
   USER_CONTROLLER_INBOUND_PORT,
@@ -10,4 +14,13 @@ export class UserController {
     @Inject(USER_CONTROLLER_INBOUND_PORT)
     private readonly userControllerInboundPort: UserControllerInboundPort,
   ) {}
+
+  @UseGuards(LoggedInGuard)
+  @Get('info/comments')
+  async readCommentsByUserId(@Users() user: GetUserIdDto) {
+    const comments = await this.userControllerInboundPort.readCommentsByUserId({
+      userId: user.id,
+    });
+    return comments;
+  }
 }
