862072dc44168ea2a4e237f6ddd74b7c02ac3811
Michael <7471919@naver.com>
2023-04-15 18:12:00 +0900

    test: Update Comment in CommentService

--- server/src/unit-test/comment/comment.service.spec.ts
+++ server/src/unit-test/comment/comment.service.spec.ts
@@ -4,12 +4,15 @@ import {
   FindCommentsByUserIdOutboundPortOutputDto,
   SaveCommentOutboundPortInputDto,
   SaveCommentOutboundPortOutputDto,
+  UpdateCommentOutboundPortInputDto,
+  UpdateCommentOutboundPortOutputDto,
 } from 'src/outbound-ports/comment/comment-repository.outbound-port';
 import { CommentService } from 'src/services/comment.service';
 
 export type MockCommentRepositoryOutboundPortParamType = {
   saveComment?: SaveCommentOutboundPortOutputDto;
   findCommentsByUserId?: FindCommentsByUserIdOutboundPortOutputDto;
+  updateComment?: UpdateCommentOutboundPortOutputDto;
 };
 
 export class MockCommentRepositoryOutboundPort
@@ -31,6 +34,12 @@ export class MockCommentRepositoryOutboundPort
     params: FindCommentsByUserIdOutboundPortInputDto,
   ): Promise<FindCommentsByUserIdOutboundPortOutputDto> {
     return this.result.findCommentsByUserId;
+  }
+
+  async updateComment(
+    params: UpdateCommentOutboundPortInputDto,
+  ): Promise<UpdateCommentOutboundPortOutputDto> {
+    return this.result.updateComment;
   }
 }
 
@@ -60,5 +69,21 @@ describe('CommentService Spec', () => {
     });
 
     expect(res).toStrictEqual(comment);
+  });
+
+  test('Update Comment', async () => {
+    const affected = { affected: 1 };
+
+    const commentService = new CommentService(
+      new MockCommentRepositoryOutboundPort({ updateComment: affected }),
+    );
+
+    const res = await commentService.updateComment({
+      commentId: '1',
+      userId: '1',
+      content: 'test comment',
+    });
+
+    expect(res).toStrictEqual({ affected: 1 });
   });
 });
